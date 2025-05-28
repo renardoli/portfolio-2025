59e397ec1c850077f834e98755986bd627f91809
Olivier Renard <renard.oliv@gmail.com>
2025-05-28 15:01:28 +0200

    feat: enhance homepage content responsiveness across viewports

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import './App.css';
 import photo from './assets/img/olivier-renard-photo.png';
 import Footer from './components/Footer';
 import Header from './components/Header';
@@ -9,17 +8,20 @@ function App() {
   return (
     <>
       <Header />
-      <main className="container px-8 py-20 flex flex-col gap-8">
-        <div className="flex gap-8">
+      <main className="container px-8 pt-5 pb-20 flex flex-col gap-8 lg:py-20">
+        <div className="flex flex-col gap-8 lg:flex-row">
+          <img
+            className="rounded-full aspect-square w-xs m-auto object-cover object-top lg:rounded-4xl lg:order-last lg:m-0 xl:w-auto xl:aspect-auto"
+            src={photo}
+            alt="Portrait d'Olivier Renard"
+          />
           <div className="flex flex-col gap-8 justify-center">
             <div>
-              <h1 className="font-bold text-8xl/21 text-dynamic-black uppercase font-stretch-semi-condensed">
+              <h1>
                 Olivier{' '}
                 <span className="font-thin font-stretch-condensed">Renard</span>
               </h1>
-              <h2 className="font-light text-4xl tracking-widest text-tobiko-orange uppercase font-stretch-condensed">
-                Développeur Fullstack Node.js/React
-              </h2>
+              <h2>Développeur Fullstack Node.js/React</h2>
             </div>
             <p>
               Mon parcours s'est construit autour d'une passion précoce pour
@@ -47,26 +49,16 @@ function App() {
               </strong>
               .
             </p>
-            <button
-              type="button"
-              className="font-normal bg-tobiko-orange w-fit px-8 py-4 rounded-full"
-            >
+            <button type="button" className="btn btn-default">
               Voir mes projets
             </button>
           </div>
-          <img
-            className="rounded-4xl object-cover"
-            src={photo}
-            alt="Portrait d'Olivier Renard"
-          />
         </div>
         <TechCarousel />
-        <div className="grid grid-cols-3 gap-8">
+        <div className="grid gap-8 grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
           {techStack.map(({ title, elements }) => (
             <div key={title}>
-              <h3 className="font-black uppercase text-4xl font-stretch-condensed border-b-4 border-tobiko-orange mb-3 pb-3">
-                {title}
-              </h3>
+              <h3>{title}</h3>
               <ul className="list-disc pl-6">
                 {elements.map((element) => (
                   <li key={element}>{element}</li>
