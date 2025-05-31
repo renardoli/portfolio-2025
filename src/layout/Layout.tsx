d3c0960558f4cf84babe78c0097109cb500d2027
Olivier Renard <renard.oliv@gmail.com>
2025-05-31 19:48:11 +0200

    feat: add framer-motion for animations and update Layout component for smooth transitions

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,15 +1,26 @@
+import { AnimatePresence, motion } from 'motion/react';
+import { useLocation } from 'react-router';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
-import type { PropsWithChildren } from 'react';
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  const location = useLocation();
 
-const Layout = ({ children }: PropsWithChildren) => {
   return (
-    <>
+    <AnimatePresence mode="wait">
       <Header />
-      <main>{children}</main>
+      <motion.main
+        key={location.pathname}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.6, ease: 'easeInOut' }}
+        className="page-content"
+      >
+        {children}
+      </motion.main>
       <Footer />
-    </>
+    </AnimatePresence>
   );
 };
 
